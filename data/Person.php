83abc9d99152aff3f6f2f493216b61cfaa498b0a
Budhi Octaviansyah <budioct@gmail.com>
2024-05-11 08:44:35 +0700

    class

---
+++ data/Person.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Apa itu Object?
+ * ● Object adalah data yang berisi field / properties / attributes dan method / function / behavior
+ *
+ * Apa itu Class?
+ * ● Class adalah blueprint, prototype atau cetakan untuk membuat Object
+ * ● Class berisikan deklarasi semua properties dan functions yang dimiliki oleh Object
+ * ● Setiap Object selalu dibuat dari Class
+ * ● Dan sebuah Class bisa membuat Object tanpa batas
+ *
+ * Membuat Class
+ * ● Untuk membuat class, kita bisa menggunakan kata kunci class
+ * ● Penamaan class biasa menggunakan format CamelCase
+ *
+ */
+
+class Person
+{
+
+}
