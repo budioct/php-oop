a6c5f6611a16c0939fcd47439cb1fd10f03202ce
Budhi Octaviansyah <budioct@gmail.com>
2024-05-11 09:20:56 +0700

    object

---
+++ Object.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Membuat Object
+ * ● Object adalah hasil instansiasi dari sebuah class
+ * ● Untuk membuat object kita bisa menggunakan kata kunci new, dan diikuti dengan nama Class dan kurung ()
+ */
+
+require_once "data\Person.php"; // import class
+
+$person1 = new Person();
+$person2 = new Person();
+var_dump($person1);
+var_dump($person2);
+
+/**
+ * result
+ * object(Person)#1 (0) {
+ * }
+ * object(Person)#2 (0) {
+ * }
+ */
